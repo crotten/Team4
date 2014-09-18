40a09c03b17f01736795a21ce6e27bdafadf449d
Margaret Godowns <margaretgodowns@gmail.com>
2014-09-18 12:12:45 -0400

    minor

--- app/assets/javascripts/controllers/good/helpFeedCtrl.js
+++ app/assets/javascripts/controllers/good/helpFeedCtrl.js
@@ -1,27 +1,27 @@
 angular.module("helpFeed")
-    .controller("helpFeedCtrl", function ($scope,$log,$location, $routeParams, $rootScope, $route, helpFeedSvc) {
-      $scope.currentUser = currentUser;
-      $scope.helps = [];
-      helpFeedSvc.getHelps().then(function (helps) {
-        $scope.helps = helps.data;
-      });
+.controller("helpFeedCtrl", function ($scope,$log,$location, $routeParams, $rootScope, $route, helpFeedSvc) {
+  $scope.currentUser = currentUser;
+  $scope.helps = [];
+  helpFeedSvc.getHelps().then(function (helps) {
+    $scope.helps = helps.data;
+  });
 
-      helpFeedSvc.getHelp($scope.currentUser.id).then(function (response) {
-        $scope.singleHelp = response.data;
-      });
+  helpFeedSvc.getHelp($scope.currentUser.id).then(function (response) {
+    $scope.singleHelp = response.data;
+  });
 
 
-      $scope.addHelp = function (helpObj) {
-        var newHelp = {
-          date:new Date(),
-          problem:helpObj.problem,
-          landmark:helpObj.landmark,
-          comments: []
-        };
-          helpFeedSvc.addHelp(newHelp)
-          $scope.helps.push(newHelp);
-          $log.info(newHelp);
-          $scope.newHelp = {};
-        };
+  $scope.addHelp = function (helpObj) {
+    var newHelp = {
+      date:new Date(),
+      problem:helpObj.problem,
+      landmark:helpObj.landmark,
+      comments: []
+    };
+    helpFeedSvc.addHelp(newHelp)
+    $scope.helps.push(newHelp);
+    $log.info(newHelp);
+    $scope.newHelp = {};
+  };
 
-    });
+});
