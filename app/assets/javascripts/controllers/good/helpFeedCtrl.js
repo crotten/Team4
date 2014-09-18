1b66130d3ef061c9dc940d5b69a21f50d459b337
Margaret Godowns <margaretgodowns@gmail.com>
2014-09-18 15:06:50 -0400

    fixed merge conflict

--- app/assets/javascripts/controllers/good/helpFeedCtrl.js
+++ app/assets/javascripts/controllers/good/helpFeedCtrl.js
@@ -1,12 +1,4 @@
 angular.module("helpFeed")
-<<<<<<< HEAD
-.controller("helpFeedCtrl", function ($scope,$log,$location, $routeParams, $rootScope, $route, helpFeedSvc) {
-  $scope.currentUser = currentUser;
-  $scope.helps = [];
-  helpFeedSvc.getHelps().then(function (helps) {
-    $scope.helps = helps.data;
-  });
-=======
     .controller("helpFeedCtrl", function ($scope,$log,$location, $routeParams, $rootScope, $route, helpFeedSvc) {
       $scope.currentUser = currentUser;
       console.log($scope.currentUser);
@@ -14,28 +6,13 @@ angular.module("helpFeed")
       helpFeedSvc.getHelps().then(function (helps) {
         $scope.helps = helps.data;
       });
->>>>>>> e38943d23e19469b11ec7f94f2a110122b2aa6ba
 
-  helpFeedSvc.getHelp($scope.currentUser.id).then(function (response) {
-    $scope.singleHelp = response.data;
-  });
+      helpFeedSvc.getHelp($scope.currentUser.id).then(function (response) {
+        $scope.singleHelp = response.data;
+      });
 
 
-<<<<<<< HEAD
-  $scope.addHelp = function (helpObj) {
-    var newHelp = {
-      date:new Date(),
-      problem:helpObj.problem,
-      landmark:helpObj.landmark,
-      comments: []
-    };
-    helpFeedSvc.addHelp(newHelp)
-    $scope.helps.push(newHelp);
-    $log.info(newHelp);
-    $scope.newHelp = {};
-  };
-=======
-      $scope.addHelp = function (helpObj) {
+      $scope.addHelp = function (helpObj, id) {
         var newHelp = {
           date:new Date(),
           problem:helpObj.problem,
@@ -47,6 +24,5 @@ angular.module("helpFeed")
           $log.info(newHelp);
           $scope.newHelp = {};
         };
->>>>>>> e38943d23e19469b11ec7f94f2a110122b2aa6ba
 
-});
+    });
